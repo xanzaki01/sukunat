ddbd8ca3c2f91c574f43cdbf9c0d4e3a39968885
xanzaki01 <webdev00003@gmail.com>
2025-11-17 20:08:33 +0500

    Initial Commit

---
+++ sukunat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sukunat
+//
+//  Created by Xan Xanzaki on 17/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
